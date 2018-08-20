8b3ad509adc8a17b2ee5a4d4d8bf8ddf80f4655e
Chad Mitzel <profMittens@users.noreply.github.com>
2018-08-19 21:03:03 -0400

    Removed point values for tests

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -11,12 +11,9 @@ for f in $CWD/*.out; do
         FILE=$(echo $FILE | cut -d '.' -f1)
         echo "[-] See tests/$FILE.cpp to see test case that caused failure"
         echo "[-] Exiting......"
-        echo "Max Potential Points: $TOTAL_POINTS"
         exit 1
     else
         TOTAL_POINTS=$(($TOTAL_POINTS+$?))
     fi
 done
-
-echo "[#] Max Potential Points: $TOTAL_POINTS"
 
